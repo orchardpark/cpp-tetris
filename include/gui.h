624f9c42de690494fc82b65577f02791cf1d8be2
niels <nielsfreelancer7750@gmail.com>
2021-01-28 20:48:34 +0100

    Add documentation

--- include/gui.h
+++ include/gui.h
@@ -13,12 +13,25 @@ public:
 	void Run();
 	void Update(const GameState& state);
 private:
+    /// Start a new game
+    /// \param game The game to start
     static void StartGame(std::shared_ptr<Game> game);
+    /// Starts the keyboard event queue and handler in a new thread
+    /// \param game The game to apply the key events to
     static void RunKeyboardController(std::shared_ptr<Game> game);
     std::string StateToString(const GameState& state);
+    /// Renders the current state of the game to screen
+    /// \param state The current game state
     void RenderState(const GameState& state);
+    /// Renders text to the screen
+    /// \param x x-Coordinate of render
+    /// \param y y-Coordinate of render
+    /// \param text The text to be rendered
+    /// \param scaleFactor The size of the render
     void RenderText(int x, int y, const char* text, float scaleFactor);
+    /// Initialized SDL2 resources
     void InitializeSDL2();
+    /// Frees all SDL2 resources
     void DeInitializeSDL2();
 
     SDL_Window* gWindow = nullptr;
